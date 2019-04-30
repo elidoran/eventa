c7dc0e4f5084301e130135d88c0bb74e50754176
Eli Doran <eli@elidoran.com>
2019-04-30 19:31:06 -0400

    configure eslint

---
+++ build/eslint/eslint.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: "2017",
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  rules: {
+    indent: [ 'error', 2, { SwitchCase: 1 } ],
+    'linebreak-style': [ 'error', 'unix' ],
+    quotes: [ 'error', 'single' ],
+    semi: [ 'error', 'never' ],
+    'array-bracket-spacing': [2, 'never'],
+    'block-scoped-var': 2,
+    'brace-style': [2, 'stroustrup'],
+    camelcase: 1,
+    'computed-property-spacing': [2, 'never'],
+    curly: 2,
+    'eol-last': 2,
+    eqeqeq: [2, 'smart'],
+    'max-depth': [1, 3],
+    'max-len': [1, 120],
+    'max-statements': [1, 30],
+    'new-cap': 1,
+    'no-extend-native': 2,
+    'no-mixed-spaces-and-tabs': 2,
+    'no-trailing-spaces': 2,
+    'no-unused-vars': 1,
+    'no-use-before-define': [2, 'nofunc'],
+    'object-curly-spacing': [2, 'always'],
+    quotes: [2, 'single', 'avoid-escape'],
+    'keyword-spacing': 2,
+    'space-unary-ops': 2,
+    'no-console': 'off',
+
+    'prefer-const': ["error", {
+        "destructuring": "any",
+        "ignoreReadBeforeAssign": false
+    }],
+  },
+}
